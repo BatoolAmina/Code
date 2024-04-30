b4af9413b32eafc91bef36903d81780d65e23362
Batool Amina <85053802+BatoolAmina@users.noreply.github.com>
2024-04-30 02:39:18 -0700

    Create Prg1.java

---
+++ Prg1.java
@@ -0,0 +1,23 @@
+import java.applet.*;
+import java.awt.*;
+public class extends applet{
+    public void paint(Graphics g){
+        Font f = new Font("Times New Roman", Font.BOLD, 40);
+        g.setFont(f);
+        g.drawString("Welcome to Applet", 100, 100);
+    }
+}
+
+// Compiling javac Test.java
+
+// Create HTML file:
+
+<html>
+<head>
+<title>Applet</title>
+</head>
+<applet code = "Test.class" width="600" height="400"></applet>
+</html>
+
+//Now open html file
+//applet viewer.html
